c46a1804b4da3d31087105ddd0bf831acf5ce424
Arthur Laurent <arthur.laurent@wifirst.fr>
2020-08-20 14:45:21 +0200

    Add terraform load_balancer conf with digital ocean

---
+++ terraform/digital_ocean_load_balancer.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_loadbalancer" "public" {
+    name   = "kubernetes-load-balancer"
+    region = "lon1"
+
+    forwarding_rule {
+        entry_port     = 80
+        entry_protocol = "tcp"
+        target_port     = 80
+        target_protocol = "tcp"
+    }
+
+    forwarding_rule {
+        entry_port     = 443
+        entry_protocol = "tcp"
+        target_port     = 443
+        target_protocol = "tcp"
+    }
+
+    healthcheck {
+        port     = 22
+        protocol = "tcp"
+    }
+
+    droplet_ids = [for node in digitalocean_kubernetes_cluster.my_cluster.node_pool[0].nodes: node.droplet_id]
+}
