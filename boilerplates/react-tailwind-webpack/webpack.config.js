2500979f32f6ebaa0e78af248e41ef598402ae3d
Arthur Laurent <laurent.arthur75@gmail.com>
2021-12-25 13:19:17 +0100

    Update react tailwind webpack boilerplate

--- boilerplates/react-tailwind-webpack/webpack.config.js
+++ boilerplates/react-tailwind-webpack/webpack.config.js
@@ -1,49 +1,89 @@
 const path = require( 'path' );
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: [
-    './src/index.js',
-    './src/style.css'
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  mode: 'development',
-  devServer: {
-    host: "0.0.0.0",
-    port: 5000,
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = (env) => {
+  const isProduction = env.production === true
+
+  let config = {
+    entry: [
+      './src/index.js',
+      './src/styles/main.css'
+    ],
+    output: {
+      filename: isProduction ? 'bundle.[fullhash].js': 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      hashFunction: "xxhash64"
+    },
+    resolve: {
+      modules: [
+        path.resolve(__dirname, 'src'),
+        'node_modules'
+      ]
+    },
+    mode: isProduction ? "production" : 'development',
+    devServer: {
+      host: "0.0.0.0",
+      port: 5000,
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'postcss-loader'
+          ]
+        },
+        {
+          test: /\.(png|jpg)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: './images/[name][ext]',
+          }
+        },
+        {
+          test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          type: 'asset/resource',
+          generator: {
+            filename: './fonts/[name][ext]',
+          }
         }
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        use: [
-          'style-loader', 'css-loader', 'postcss-loader'
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanAfterEveryBuildPatterns: ['dist']
+      }),
+      new HtmlWebPackPlugin({
+        filename: "./index.html",
+        template: "./src/index.html"
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './src/runtime-config.js' }
         ]
-      }
+      })
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
-};
+  }
+
+  if(isProduction) {
+    config.plugins.unshift(
+      new MiniCssExtractPlugin({
+        filename: 'styles.[fullhash].css'
+      })
+    )
+  }
+
+  return config
+}
